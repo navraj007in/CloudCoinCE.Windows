09781984c5b83fef0ae9af91e0edf8269fa6a816
navraj.singh <navraj.singh@infodartmail.com>
2017-10-04 00:33:59 +0530

    Images and Resources addition

--- CloudCoinCE/App.xaml.cs
+++ CloudCoinCE/App.xaml.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using Founders;
 
 namespace CloudCoinCE
 {
@@ -13,5 +14,6 @@ namespace CloudCoinCE
     /// </summary>
     public partial class App : Application
     {
+  
     }
 }
